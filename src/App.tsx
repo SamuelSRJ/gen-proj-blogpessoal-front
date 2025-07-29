f565e6653a669f9a273045b444bd3aedc3364e55
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-07-29 19:12:14 -0300

    feat: Criar a camada de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Login from './pages/login/Login'
+import Navbar from './components/navbar/Navbar'
+import { AuthProvider } from './contexts/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 
 function App() {
   return (
-    <Home />
+    <>
+      <div className=''>
+        <AuthProvider>
+          <BrowserRouter>
+            <Navbar />
+            <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/cadastro' element={<Cadastro />} />
+                <Route path='/login' element={<Login />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
+      </div>
+    </>
   )
 }
 
